4a7b48290eebce9ef228bf8694edca917f666289
Uwana <uwikaiddi@gmail.com>
2019-04-10 20:20:02 -0500

    Add complete index route, add additional characters

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -1,2 +1,22 @@
 class Api::CharactersController < ApplicationController
+  def index
+    @characters = Character.all
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,10 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Character.create([
+#   {
+#     first_name: "Yuki",
+#     last_name: "Sohma",
+#     color: '#ffffff'
+#   }
+# ])
