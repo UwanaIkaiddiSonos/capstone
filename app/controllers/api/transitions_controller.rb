a53fe3fcadfea1d6c53ad19345ae9b5a7c44c4bd
Uwana <uwikaiddi@gmail.com>
2019-04-14 15:56:33 -0500

    Add transition index, show, delete routes

--- app/controllers/api/transitions_controller.rb
+++ app/controllers/api/transitions_controller.rb
@@ -1,2 +1,34 @@
 class Api::TransitionsController < ApplicationController
+  def index
+    @transitions = Transition.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @transition = Transition.find_by(id: params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def create
+    @transition = Transition.new(
+      name: params[:input_name]
+    )
+    @transition.save
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @transition = Transition.find_by(id: params[:id])
+
+    @transition.update(
+      url: params[:input_name] || @transition.name
+    )
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    @transition = Transition.find_by(id: params[:id])
+    @transition.destroy
+    render "destroy.json.jbuilder"
+  end
 end
