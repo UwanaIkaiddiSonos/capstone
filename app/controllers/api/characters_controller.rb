ffab1f747da0d3a615eb11f6633ee0519bc49df4
Uwana <uwikaiddi@gmail.com>
2019-04-14 14:14:27 -0500

    Add update and destroy, delete create.json.jbuilder

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -10,19 +10,30 @@ class Api::CharactersController < ApplicationController
   end
 
   def create
-    @character = Character.create(
+    @character = Character.new(
       first_name: params[:input_first_name],
       last_name: params[:input_last_name],
       color: params[:input_color]
     )
-    render 'create.json.jbuilder'
+
+    @character.save
+    render 'show.json.jbuilder'
   end
 
   def update
-    
+    @character = Character.find_by(id: params[:id])
+
+    @character.update(
+      first_name: params[:input_first_name] || @character.first_name,
+      last_name: params[:input_last_name] || @character.last_name,
+      color: params[:input_color] || @character.color
+    )
+    render 'show.json.jbuilder'
   end
 
   def destroy
-    
+    @character = Character.find_by(id: params[:id])
+    @character.destroy
+    render "destroy.json.jbuilder"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     get "/characters" => 'characters#index'
     post "/characters" => 'characters#create'
     get "/characters/:id" => 'characters#show'
-    patch "/characters" => 'characters#update'
-    delete "/characters" => 'characters#destroy'
+    patch "/characters/:id" => 'characters#update'
+    delete "/characters/:id" => 'characters#destroy'
   end
 end
