494af71139fff3d5032ef1868435de05ae496f6f
Uwana <uwikaiddi@gmail.com>
2019-04-30 14:59:26 -0500

    Remove final view from Label controller

--- app/controllers/api/labels_controller.rb
+++ app/controllers/api/labels_controller.rb
@@ -1,18 +1,4 @@
 class Api::LabelsController < ApplicationController
-  def final
-    # name = params[:name]
-    # hex = params[:hex]
-    @result = ""
-    @result += add_character("Sylvie", "333333")
-    @result += add_label("meadow")
-    @result += add_scene("school", "fade")
-    @result += show_character("sylvie green smile")
-    @result += dialogue("s", "Hi, I'm Sylvie!")
-    @result += menu("lunch", "Where should we eat?", "Grab a burger", "burger", "Get tacos", "tacos")
-
-    render 'final.json.jbuilder'
-  end
-
   def index
     @labels = Label.all
     render 'index.json.jbuilder'
@@ -24,13 +10,10 @@ class Api::LabelsController < ApplicationController
   end
 
   def create
-    @image_name = params[:input_image_name]
-    # @label = Label.new(
-    #   name: params[:input_name]
-    # )
-
-    # @label.save
-
+    @label = Label.new(
+      name: params[:input_name]
+    )
+    @label.save
     render 'show.json.jbuilder'
   end
 
@@ -47,49 +30,5 @@ class Api::LabelsController < ApplicationController
     @label = Label.find_by(id: params[:id])
     @label.destroy
     render "destroy.json.jbuilder"
-  end
-
-  def add_character(character, color_hex)
-    final_result = ""
-    @short_name = character[0].downcase
-    final_result += "# Characters used in-game\n"
-    final_result += "define #{character[0].downcase} = Character(_(\"#{character}\"), color=\"#{color_hex}\")\n"
-    return final_result 
-  end
-
-  # def add_music(music_file)
-  #   final_result = ""
-  #   final_result += "play music \"#{music_file}\""
-  # end
-
-  def add_scene(background, transition)
-    final_result = ""
-    final_result += "scene bg #{background}\n"
-    final_result += "with #{transition}\n"
-  end
-
-  def add_label(label_name)
-    final_result = ""
-    final_result += "label #{label_name.downcase}\n"
-  end
-
-  def dialogue(short_name, narration)
-    final_result = ""
-    final_result += "\t#{short_name} #{narration}\n"
-  end
-
-  def menu(menu_name, intro_text, choice1, label1, choice2, label2)
-    final_result = ""
-    final_result += "\tmenu #{menu_name}\n"
-    final_result += "\t\t#{intro_text}\n"
-    final_result += "\t\t#{choice1}\n"
-    final_result += "\t\t\tjump #{label1}\n"
-    final_result += "\t\t#{choice2}\n"
-    final_result += "\t\t\tjump #{label2}\n"
-  end
-
-  def show_character(image_name)
-    final_result = ""
-    final_result += "\tshow #{image_name}\n"
   end
 end
