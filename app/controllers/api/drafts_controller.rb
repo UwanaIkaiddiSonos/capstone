a11ef261553bf96cc44d46a9af406514be21b05b
Uwana <uwikaiddi@gmail.com>
2019-05-05 10:23:00 -0500

    Various changes

--- app/controllers/api/drafts_controller.rb
+++ app/controllers/api/drafts_controller.rb
@@ -37,45 +37,57 @@ class Api::DraftsController < ApplicationController
   # Unrendered methods
   def add_character(character, color_hex)
     final_result = ""
-    @short_name = character[0].downcase
-    final_result += "# Characters used in-game\n"
-    final_result += "define #{character[0].downcase} = Character(_(\"#{character}\"), color=\"#{color_hex}\")\n"
+    if character[0] != " "
+      @short_name = character[0].downcase
+      final_result += "# Characters used in-game\n"
+      final_result += "define #{character[0].downcase} = Character(_(\"#{character}\"), color=\"#{color_hex}\")\n"      
+    end
     return final_result 
   end
 
   def add_label(label_name)
     final_result = ""
     final_result += "label #{label_name}\n"
+    return final_result
   end
 
   def add_scene(background, transition)
     final_result = ""
-    final_result += "scene bg #{background}\n"
-    final_result += "with #{transition}\n"
+    if background != " "
+      final_result += "scene bg #{background}\n"
+      final_result += "with #{transition}\n"
+    end
+    return final_result
   end
 
-  def add_music(music_file)
-    final_result = ""
-    final_result += "play music \"#{music_file}\""
-  end
+  # def add_music(music_file)
+  #   final_result = ""
+  #   final_result += "play music \"#{music_file}\""
+  # end
 
-  def show_character(image_name)
-    final_result = ""
-    final_result += "\tshow #{image_name}\n"
-  end
+  # def show_character(image_name)
+  #   final_result = ""
+  #   final_result += "\tshow #{image_name}\n"
+  # end
 
   def dialogue(short_name, narration)
     final_result = ""
-    final_result += "\t#{short_name} #{narration}\n"
+    if narration != " "
+      final_result += "\t#{short_name} #{narration}\n"
+    end
+    return final_result
   end
 
   def menu(menu_name, intro_text, choice1, label1, choice2, label2)
     final_result = ""
-    final_result += "\tmenu #{menu_name}\n"
-    final_result += "\t\t#{intro_text}\n"
-    final_result += "\t\t#{choice1}\n"
-    final_result += "\t\t\tjump #{label1}\n"
-    final_result += "\t\t#{choice2}\n"
-    final_result += "\t\t\tjump #{label2}\n"
+    if menu_name != " "
+      final_result += "\tmenu #{menu_name}\n"
+      final_result += "\t\t#{intro_text}\n"
+      final_result += "\t\t#{choice1}\n"
+      final_result += "\t\t\tjump #{label1}\n"
+      final_result += "\t\t#{choice2}\n"
+      final_result += "\t\t\tjump #{label2}\n"
+    end
+    return final_result
   end
 end
