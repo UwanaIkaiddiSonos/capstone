01fab896154bba9fcf03ed1b345b3b57e50cc30d
Uwana <uwikaiddi@gmail.com>
2019-05-08 15:37:16 -0500

    Changes to drafts controller methods

--- app/controllers/api/drafts_controller.rb
+++ app/controllers/api/drafts_controller.rb
@@ -9,28 +9,40 @@ class Api::DraftsController < ApplicationController
   #   #render 'show.json.jbuilder'
   # end
   def convert
-    first_name = params[:input_first_name]
-    short_name = first_name[0].downcase
-    color = params[:input_color]
-    label = params[:input_label_name]
-    p label
-    scene = params[:input_scene_name]
-    transition = params[:input_transition_name]
-    dialogue = params[:input_dialogue]
-    menu = params[:input_menu]
-    intro = params[:input_intro]
-    choice1_label = params[:input_choice1_label]
-    choice1 = params[:input_choice1]
-    choice2_label = params[:input_choice2_label]
-    choice2 = params[:input_choice2]
+    # first_name = params[:input_first_name]
+    # short_name = first_name[0].downcase
+    # color = params[:input_color]
+    # label = params[:input_label_name]
+    # p label
+    # scene = params[:input_scene_name]
+    # transition = params[:input_transition_name]
+    # dialogue = params[:input_dialogue]
+    # menu = params[:input_menu]
+    # intro = params[:input_intro]
+    # choice1_label = params[:input_choice1_label]
+    # choice1 = params[:input_choice1]
+    # choice2_label = params[:input_choice2_label]
+    # choice2 = params[:input_choice2]
+    inputs = params[:fieldInputs]
 
     @result = ""
-    @result += add_character(first_name, color)
-    @result += add_label(label)
-    @result += add_scene(scene, transition)
+    inputs.each do |input|
+      if input['type'] == 'Label'
+        @result += add_label(input["userInput"])
+      elsif input['type'] == 'Dialogue'
+        @result += dialogue(input['character'][0].downcase, input['userInput'])
+      elsif input['type'] == 'Menu'
+        @result += menu(input['intro'], input['choice1'], input['choice1_label'], input['choice2'], input['choice2_label'])
+      elsif input['type'] == 'Scene'
+        @result += add_scene(input['userInput'])
+      elsif input['type'] == 'Transition'
+        @result += add_transition(input['userInput'])
+      elsif input['type'] == 'Character'
+        @result += add_character(input['userInput'], input['color'])
+      end
+    end
+
     # @result += show_character("sylvie green smile")
-    @result += dialogue(short_name, dialogue)
-    @result += menu(menu, intro, choice1_label, choice1, choice2_label, choice2)
 
     render 'convert.json.jbuilder'
   end
@@ -51,12 +63,15 @@ class Api::DraftsController < ApplicationController
     return final_result
   end
 
-  def add_scene(background, transition)
+  def add_scene(background)
     final_result = ""
-    if background != " "
-      final_result += "\tscene bg #{background}\n"
-      final_result += "\twith #{transition}\n"
-    end
+    final_result += "\tscene bg #{background}\n"
+    return final_result
+  end
+
+  def add_transition(transition)
+    final_result = ""
+    final_result += "\twith #{transition}\n" 
     return final_result
   end
 
@@ -78,16 +93,14 @@ class Api::DraftsController < ApplicationController
     return final_result
   end
 
-  def menu(menu_name, intro_text, choice1, label1, choice2, label2)
+  def menu(intro_text, choice1, label1, choice2, label2)
     final_result = ""
-    if menu_name != " "
-      final_result += "\tmenu #{menu_name}:\n"
-      final_result += "\t\t\"#{intro_text}\"\n"
-      final_result += "\t\t\"#{choice1}\":\n"
-      final_result += "\t\t\tjump #{label1}\n"
-      final_result += "\t\t\"#{choice2}\":\n"
-      final_result += "\t\t\tjump #{label2}\n"
-    end
+    final_result += "\tmenu:\n"
+    final_result += "\t\t\"#{intro_text}\"\n"
+    final_result += "\t\t\"#{choice1}\":\n"
+    final_result += "\t\t\tjump #{label1}\n"
+    final_result += "\t\t\"#{choice2}\":\n"
+    final_result += "\t\t\tjump #{label2}\n"
     return final_result
   end
 end
